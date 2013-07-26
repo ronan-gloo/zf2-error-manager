a6ee1f9b72a2ff6179283a64eda4e7ef214947c8
Ronan <tessierronan@yahoo.fr>
2013-07-26 20:45:33 +0200

    up

--- config/module.config.php
+++ config/module.config.php
@@ -23,7 +23,11 @@ return [
         'assets' =>
         [
             //'css' => ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css'],
-            //'js'  => ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js']
+            'js'  =>
+            [
+                //'//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js'
+                'https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js'
+            ]
         ]
     ],
 
